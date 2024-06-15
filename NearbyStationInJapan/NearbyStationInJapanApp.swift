c21f527635a868932cce40be44ab7dc9514dfa0b
aicccux <72127637+aicccux@users.noreply.github.com>
2024-06-15 11:01:51 +0900

    Initial Commit

---
+++ NearbyStationInJapan/NearbyStationInJapanApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearbyStationInJapanApp.swift
+//  NearbyStationInJapan
+//
+//  Created by aicccux on 6/15/24.
+//
+
+import SwiftUI
+
+@main
+struct NearbyStationInJapanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
